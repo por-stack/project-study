47cd6acf273f626da71e08608ff391a6043cd651
por-stack <PorschePanamera2019>
2019-11-18 17:45:41 +0100

    IoPusho

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -81,7 +81,7 @@ public class Factory {
 	}
 
 	/*
-	 * put Raster into zones used 
+	 * put Raster into zones used
 	 */
 	public void rasterIntoZones(int rowInImport) {
 		for (int i = 1; i < mport.getI() - 1; i++) { // j=5
@@ -96,46 +96,63 @@ public class Factory {
 				}
 			}
 
-			//find out in which row the zone has to go. Remember that each zones involves to rows of Rasters
+			// find out in which row the zone has to go. Remember that each zones involves
+			// to rows of Rasters
 			int rowInLayout;
+			boolean rowEven = false;
 			if (rowNumber % 2 == 0) {
+				rowEven = true;
 				rowInLayout = rowNumber;
 				rowInLayout = (rowInLayout + 2) / 2;
 			} else {
 				rowInLayout = rowNumber;
 				rowInLayout = (rowInLayout + 1) / 2;
 			}
-			
-			//get the zone for this specific raster
+
+			// get the zone for this specific raster
 			String zoneName = matrix[rowInImport][1];
-			
-			//see if this zone is already in the factoryLayout. if not, add the zone and add the raster into this zone. 
-			//if yes, get into zone and add the new raster in its interal layout. 
+
+			// see if this zone is already in the factoryLayout. if not, add the zone and
+			// add the raster into this zone.
+			// if yes, get into zone and add the new raster in its interal layout.
 			boolean alreadyIn = false;
 			int k;
 			for (k = 0; k < 7; k++) {
-				if (factoryStructure[rowInLayout][6-k] == null) {
+				if (factoryStructure[rowInLayout][6 - k] == null) {
 					break;
 				} else {
-				if (factoryStructure[rowInLayout][6-k].equals(zoneName)) {
-					alreadyIn = true; 
-					break; 
+					if (factoryStructure[rowInLayout][6 - k].equals(zoneName)) {
+						alreadyIn = true;
+						break;
+					}
 				}
-			}
-			
-			if (k != 7) {
-				if (alreadyIn == false) {
-					factoryStructure[rowInLayout][6-k] = new Zone(zoneName);
-					factoryStructure[rowInLayout][6-k].raster = new Raster[2][43];
-					
+
+				if (k != 7) {
+					if (alreadyIn == false) {
+						factoryStructure[rowInLayout][6 - k] = new Zone(zoneName);
+						int sizeRaster = Integer.parseInt(matrix[rowInImport][17]);
+						factoryStructure[rowInLayout][6 - k].raster = new Raster[2][43];
+						int firstOrSecondRow;
+						if (rowEven == true) {
+							firstOrSecondRow = 0;
+							factoryStructure[rowInLayout][6 - k].amountRasterRow1 += sizeRaster;
+						} else {
+							firstOrSecondRow = 1;
+							factoryStructure[rowInLayout][6 - k].amountRasterRow2 += sizeRaster;
+						}
+						factoryStructure[rowInLayout][6 - k].raster[firstOrSecondRow][42
+								- factoryStructure[rowInLayout][6 - k].amountRasterRow1] = new Raster(rowNumber,
+										columnNumber);
+
+					}
 				}
-			}
-			
 
 //			factoryStructure[j][factoryStructure[0].length - 1 + 12 - columnNumber] = new Raster(rowNumber,
 //					columnNumber);
 //			System.out.println(factoryStructure[j][factoryStructure[0].length - 1 + 12 - columnNumber]);
+			}
 		}
+
 	}
 
 }
