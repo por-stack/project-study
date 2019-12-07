54d7dab968643037d375d99edf5b3eb6c152e76f
por-stack <PorschePanamera2019>
2019-12-07 15:58:02 +0100

    Excelö

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -301,20 +301,20 @@ public class Factory {
 		this.zonesToAllocate = zonesToAllocate;
 	}
 
-	public static void main(String[] args) throws InvalidFormatException, IOException {
-		Factory initial = new Factory();
-
-		Zone[][] factoryStructure = initial.getFactoryStructure();
-		for (int i = 0; i < factoryStructure.length; i++) {
-			System.out.println("\n" + "NEW ROW" + "\n");
-			for (int j = 0; j < factoryStructure[0].length; j++) {
-				if (factoryStructure[i][6 - j] == null) {
-					System.out.println("null");
-				} else {
-					System.out.println(factoryStructure[i][6 - j].name);
-					System.out.println(Arrays.deepToString(factoryStructure[i][6 - j].raster));
-				}
-			}
-		}
-	}
+//	public static void main(String[] args) throws InvalidFormatException, IOException {
+//		Factory initial = new Factory();
+//
+//		Zone[][] factoryStructure = initial.getFactoryStructure();
+//		for (int i = 0; i < factoryStructure.length; i++) {
+//			System.out.println("\n" + "NEW ROW" + "\n");
+//			for (int j = 0; j < factoryStructure[0].length; j++) {
+//				if (factoryStructure[i][6 - j] == null) {
+//					System.out.println("null");
+//				} else {
+//					System.out.println(factoryStructure[i][6 - j].name);
+//					System.out.println(Arrays.deepToString(factoryStructure[i][6 - j].raster));
+//				}
+//			}
+//		}
+//	}
 }
